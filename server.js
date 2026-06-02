d6373ebc1f6c21cdde0691abafbab085c4836127
navnet1 <kitunava0@gmail.com>
2026-06-02 09:01:08 +0530

    Initial backend

---
+++ server.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const cors = require("cors");
+
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+let messages = [];
+
+app.get("/messages", (req, res) => {
+    res.json(messages);
+});
+
+app.post("/messages", (req, res) => {
+    messages.push(req.body.message);
+    res.json({ success: true });
+});
+
+app.listen(3000, () => {
+    console.log("Server running");
+});
